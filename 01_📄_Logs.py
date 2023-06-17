967c7d6bca11a26a5dd48d3401ed47d9c858e0be
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-17 08:39:49 +0000

    debug

--- 01_📄_Logs.py
+++ 01_📄_Logs.py
@@ -12,20 +12,22 @@ usernames = [user["usernames"] for user in users.data]
 names = [user["names"] for user in users.data]
 hashed_passwords = [user["passwords"] for user in users.data]
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "logs_cookie", "abcd", 14)
+st.write(names)
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+# authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+#     "logs_cookie", "abcd", 14)
 
-if authentication_status is False:
-    st.error("Username/password is incorrect")
+# name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status is None:
-    st.warning("Please enter your username and password")
+# if authentication_status is False:
+#     st.error("Username/password is incorrect")
 
-if authentication_status:
-    st.title("📊 {name}'s Dashboard")
-    st.write("Some Dashboard")
+# if authentication_status is None:
+#     st.warning("Please enter your username and password")
 
-    st.title("📄 Your Logs ✍")
-    st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
+# if authentication_status:
+#     st.title("📊 {name}'s Dashboard")
+#     st.write("Some Dashboard")
+
+#     st.title("📄 Your Logs ✍")
+#     st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")

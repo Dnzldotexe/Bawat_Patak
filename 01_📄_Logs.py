4aa9d25d445594197ca663bbf3051b63e414af0a
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-17 12:31:39 +0000

    debug

--- 01_📄_Logs.py
+++ 01_📄_Logs.py
@@ -8,9 +8,9 @@ Title()
 
 users = db.fetch_all_users()
 
-usernames = {"usernames":user['usernames'] for user in users}
-names = {"names":user['names'] for user in users}
-hashed_passwords = {"passwords":user['passwords'] for user in users}
+usernames = {"usernames":user['usernames'] for user in users.data}
+names = {"names":user['names'] for user in users.data}
+hashed_passwords = {"passwords":user['passwords'] for user in users.data}
 
 i=0
 while i < len(usernames):
@@ -19,20 +19,20 @@ while i < len(usernames):
     st.write(hashed_passwords)
     i+=1
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "logs_cookie", "abcd", 14)
+# authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+#     "logs_cookie", "abcd", 14)
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+# name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status is False:
-    st.error("Username/password is incorrect")
+# if authentication_status is False:
+#     st.error("Username/password is incorrect")
 
-if authentication_status is None:
-    st.warning("Please enter your username and password")
+# if authentication_status is None:
+#     st.warning("Please enter your username and password")
 
-if authentication_status:
-    st.title("📊 {name}'s Dashboard")
-    st.write("Some Dashboard")
+# if authentication_status:
+#     st.title("📊 {name}'s Dashboard")
+#     st.write("Some Dashboard")
 
-    st.title("📄 Your Logs ✍")
-    st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
+#     st.title("📄 Your Logs ✍")
+#     st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")

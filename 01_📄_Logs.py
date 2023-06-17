f24f89016105d9818b5c146ebb6365781f683b31
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-17 07:21:34 +0000

    added database and login page

--- 01_📄_Logs.py
+++ 01_📄_Logs.py
@@ -1,8 +1,31 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 from modules.formatter import Title
+from modules import database as db
 
 # Title page
 Title()
 
-st.title("📄 Your Logs:")
-st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
+users = db.fetch_all_users()
+
+usernames = [{user["usernames"]} for user in users.data]
+names = [{user["names"]} for user in users.data]
+hashed_passwords = [{user["passwords"]} for user in users.data]
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "logs_cookie", "random_cookey", 14)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+    st.title(f"📊 {name}'s Dashboard")
+    st.write("Some Dashboard")
+
+    st.title("📄 Your Logs ✍")
+    st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
---
+++ modules/database.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from supabase import create_client, Client
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url, key)
+
+supabase = init_connection()
+
+def create_user(username, name, password):
+    res = supabase.auth.sign_up({
+    "usernames": f'{username}',
+    "names": f'{name}',
+    "passwords": f'{stauth.Hasher(password).generate()}',
+    })
+    return res
+
+@st.cache_data(ttl=600)
+def fetch_all_users():
+    return supabase.table("users_db").select("*").execute()
+
+# def update_password():
+#     return supabase.table("users_db").select("*").execute()
+
+# def delete_user():
+#     return supabase.table("users_db").select("*").execute()

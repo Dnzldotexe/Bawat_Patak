ac76a8e01f7833d30c9729c50fa522f722d44376
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-18 15:23:09 +0000

    removed pages

--- pages/02_📚_About.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-# Setting title page
-st.set_page_config(page_title="Bawat Patak", page_icon=":droplet:")
-
-
-st.title("📚 About Bawat Patak 💧")
-st.write("about the page something.")

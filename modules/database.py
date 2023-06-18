e6df6d616d2e15d88a4318d626704b8b86235ef6
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-18 10:48:35 +0000

    updated database functions

--- modules/database.py
+++ modules/database.py
@@ -1,25 +1,41 @@
+"""
+This module handles all the functions for the database
+"""
 import streamlit as st
 import streamlit_authenticator as stauth
 from supabase import create_client#, Client
 
-# Initialize connection.
-# Uses st.cache_resource to only run once.
+
 @st.cache_resource
 def init_connection():
+    """
+    Initialize connection.
+    Uses st.cache_resource to only run once.
+    """
     url = st.secrets["supabase_url"]
     key = st.secrets["supabase_key"]
     return create_client(url, key)
 
+# Function call
 supabase = init_connection()
 
-#@st.cache_data(ttl=600)
+
+# Caching the fetched data
+@st.cache_data(ttl=600)
 def fetch_all_users():
+    """
+    Fetching all user data from the database
+    """
     return supabase.table("users_db").select("*").execute()
 
-def create_user(username, name, password):
-    res = supabase.auth.sign_up({
-    "usernames": f'{username}',
-    "names": f'{name}',
-    "passwords": f'{stauth.Hasher(password).generate()}',
-    })
-    return res
+def insert_user(username: str, name: str, password:str, cookie_name="bawat-patak_cookie", cookie_key="abcde"):
+    """
+    Creates a new user with a hashed password.
+    """
+    data, count = supabase.table('countries').insert({
+        "usernames": username, 
+        "names": name,
+        "passwords": stauth.Hasher(password).generate(),
+        "cookie_names": cookie_name,
+        "cookie_key": cookie_key,
+        }).execute()
--- pages/02_📚_About.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Title page
-title = "Bawat Patak"
-icon = "💧"
-st.set_page_config(page_title=title, page_icon=icon)
-
-st.title("📚 About Bawat Patak")

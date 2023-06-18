74180af7519b3d3b457fe43867a34748ac75273f
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-18 14:55:08 +0000

    cleaned code, updated readme

--- app.py
+++ app.py
@@ -13,25 +13,16 @@ def greet() -> str:
     current_time = datetime.datetime.now()
 
     if current_time.hour < 12:
-        return "Good Morning, "
+        return "Good Morning,"
 
     if 12 <= current_time.hour < 18:
-        return "Good Afternoon, "
+        return "Good Afternoon,"
 
-    return "Good Evening, "
+    return "Good Evening,"
 
 
 # Setting title page
 st.set_page_config(page_title="Bawat Patak", page_icon=":droplet:")
-
-
-# Fetching all the users
-users = db.fetch_all_users()
-
-# def to_list() -> list[str]:
-#     """
-#     Converting fetched data into list independent of each other
-#     """
 
 
 def create_config():
@@ -41,49 +32,51 @@ def create_config():
     Returns:
         dict()
     """
-    list_usernames = [user['usernames'] for user in users.data]
-    list_names = [user['names'] for user in users.data]
-    list_emails = [user['emails'] for user in users.data]
-    list_passwords = [user['passwords'] for user in users.data]
+
+    # Fetching all user data
+    users = db.fetch_all_users()
+
+    # Converting fetched data into list independent of each other
+    usernames = [user['usernames'] for user in users.data]
+    names = [user['names'] for user in users.data]
+    emails = [user['emails'] for user in users.data]
+    passwords = [user['passwords'] for user in users.data]
 
     # hashing passwords
-    list_hashed_passwords = stauth.Hasher(list_passwords).generate()
-
-    usernames, names, emails, passwords = list_usernames, list_names, list_emails, list_hashed_passwords
+    hashed_passwords = stauth.Hasher(passwords).generate()
 
     credentials = {"usernames":{}}
-    for username, name, email, password in zip(usernames, names, emails, passwords):
+    for username, name, email, password in zip(usernames, names, emails, hashed_passwords):
         user_dict = {"email": email, "name":name,"password":password}
         credentials["usernames"].update({username:user_dict})
 
     return credentials
 
 
-# def main():
-    # """
-    # Contains the functions of the application
-    # """
-authenticator = stauth.Authenticate(create_config(),
-    "logs_cookie", "cookie_key_abcd", 14)
+def main():
+    """
+    Contains the functions of the application
+    """
+    authenticator = stauth.Authenticate(create_config(),
+        "logs_cookie", "cookie_key_abcd", 14)
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+    name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status is False:
-    st.error("Username/Password is incorrect")
+    if authentication_status is False:
+        st.error("Username/Password is incorrect")
 
-if authentication_status is None:
-    st.warning("Please enter your username and password")
+    if authentication_status is None:
+        st.warning("Please enter your username and password")
 
-if authentication_status is True:
-    st.success(f"Welcome *{username}*")
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title(f"Welcome {name}")
-    st.title("📊 Your Dashboard")
-    st.write("Some Dashboard")
+    if authentication_status is True:
+        authenticator.logout("Logout", "sidebar")
+        st.sidebar.title(f"{greet()} {name}")
+        st.title("📊 Your Dashboard")
+        st.write("Some Dashboard")
 
-    st.title("📄 Your Logs ✍")
-    st.write("This is a placeholder.")
+        st.title("📄 Your Logs ✍")
+        st.write("This is a placeholder.")
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()

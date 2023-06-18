353d5d9b997419db88839032e16a46c4055c7b1c
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-06-18 12:35:05 +0000

    debugging, code fixes

--- 01_📄_Logs.py
+++ 01_📄_Logs.py
@@ -1,34 +1,73 @@
+"""
+This module is the main application 
+"""
 import streamlit as st
 import streamlit_authenticator as stauth
 from modules import database as db
 
-# Title page
-title = "Bawat Patak"
-icon = "💧"
+
+# Setting title page
+icon, title = '💧', 'Bawat Patak'
 st.set_page_config(page_title=title, page_icon=icon)
 
+
+# Fetching all the users
 users = db.fetch_all_users()
 
-list_usernames = [user['usernames'] for user in users.data]
-list_names = [user['names'] for user in users.data]
-list_hashed_passwords = [user['passwords'] for user in users.data]
+def to_list() -> list[str]:
+    """
+    Converting fetched data into list independent of each other
+    """
+    list_usernames = [user['usernames'] for user in users.data]
+    list_names = [user['names'] for user in users.data]
+    list_hashed_passwords = [user['hashed_passwords'] for user in users.data]
+    list_cookie_names = [user['cookie_names'] for user in users.data]
+    list_cookie_keys = [user['cookie_keys'] for user in users.data]
 
-authenticator = stauth.Authenticate(list_names, list_usernames, list_hashed_passwords,
-    "logs_cookie", "cookie_key_abcd", 14)
+    return list_usernames, list_names, list_hashed_passwords, list_cookie_names, list_cookie_keys
+                
+def create_config() -> dict(str):
+    """
+    Combining all user data into a dictionary
+    """
+    list_usernames, list_names, list_passwords, list_cookie_names, list_cookie_keys = to_list()
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+    user_values = []
+    for name, password in zip(list_names, list_passwords):
+        user_values.append({'name': name, 'password': password})
 
-if authentication_status is False:
-    st.error("Username/Password is incorrect")
+    credentials = {'usernames': {}}
+    for username, user_data in zip(list_usernames, user_values):
+        credentials['usernames'][username] = user_data
 
-if authentication_status is None:
-    st.warning("Please enter your username and password")
+    config = {
+        'credentials': credentials,
+        # 'cookie': dict(zip(list_cookie_names, list_cookie_keys))
+    }
 
-if authentication_status:
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.title(f"Welcome {name}")
-    st.title("📊 Your Dashboard")
-    st.write("Some Dashboard")
+    return config
 
-    st.title("📄 Your Logs ✍")
-    st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
+def main():
+    authenticator = stauth.Authenticate(create_config(),
+        "logs_cookie", "cookie_key_abcd", 14)
+
+    name, authentication_status, username = authenticator.login('Login', 'main')
+
+    if authentication_status is False:
+        st.error("Username/Password is incorrect")
+
+    if authentication_status is None:
+        st.warning("Please enter your username and password")
+
+    if authentication_status:
+        authenticator.logout("Logout", "sidebar")
+        st.sidebar.title(f"Welcome {name}")
+        st.title("📊 Your Dashboard")
+        st.write("Some Dashboard")
+
+        st.title("📄 Your Logs ✍")
+        st.write("This is a placeholder. I'm checking if changes reflects immediately to streamlit.")
+
+
+if __name__ == "__main__":
+    main()
--- modules/database.py
+++ modules/database.py
@@ -28,14 +28,14 @@ def fetch_all_users():
     """
     return supabase.table("users_db").select("*").execute()
 
-def insert_user(username: str, name: str, password:str, cookie_name="bawat-patak_cookie", cookie_key="abcde"):
+def insert_user(username: str, name: str, password:str, cookie_name: str="bawat-patak_cookie", cookie_key: str="abcde"):
     """
     Creates a new user with a hashed password.
     """
-    data, count = supabase.table('countries').insert({
+    data, count = supabase.table('users_db').insert({
         "usernames": username, 
         "names": name,
-        "passwords": stauth.Hasher(password).generate(),
+        "hashed_passwords": stauth.Hasher(password).generate(),
         "cookie_names": cookie_name,
-        "cookie_key": cookie_key,
+        "cookie_keys": cookie_key
         }).execute()

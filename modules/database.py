b22d1534cbdcd8925dd79df95c823c5040d606d3
Dnzl.exe <cempronziel55@gmail.com>
2023-06-25 20:03:47 +0800

    input converted to float

--- modules/database.py
+++ modules/database.py
@@ -69,7 +69,7 @@ def fetch_logs(usernames: str):
 def insert_logs(
         usernames: str,
         date: str,
-        consumption: str,
+        consumption: float,
         ) -> None:
     """
     Inserts new logs to the database
